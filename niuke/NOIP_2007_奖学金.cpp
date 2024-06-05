f2cbcec1a71cde7e375cbdd37160dea828e220b2
Yfanx <yaxuanying20@gmail.com>
2024-06-05 21:41:54 +0800

    6.5

---
+++ niuke/NOIP_2007_奖学金.cpp
@@ -0,0 +1,62 @@
+/*
+    [[ ⣿⣿⣿⣿⣯⣿⣿⠄⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠈⣿⣿⣿⣿⣿⣿⣆⠄ ]],
+    [[ ⢻⣿⣿⣿⣾⣿⢿⣢⣞⣿⣿⣿⣿⣷⣶⣿⣯⣟⣿⢿⡇⢃⢻⣿⣿⣿⣿⣿⢿⡄ ]],
+    [[ ⠄⢿⣿⣯⣏⣿⣿⣿⡟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣧⣾⢿⣮⣿⣿⣿⣿⣾⣷ ]],
+    [[ ⠄⣈⣽⢾⣿⣿⣿⣟⣄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣝⣯⢿⣿⣿⣿⣿⣿ ]],
+    [[ ⣿⠟⣫⢸⣿⢿⣿⣾⣿⢿⣿⣿⢻⣿⣿⣿⢿⣿⣿⣿⢸⣿⣼⣿⣿⣿⣿⣿⣿⣿ ]],
+    [[ ⡟⢸⣟⢸⣿⠸⣷⣝⢻⠘⣿⣿⢸⢿⣿⣿⠄⣿⣿⣿⡆⢿⣿⣼⣿⣿⣿⣿⢹⣿ ]],
+    [[ ⡇⣿⡿⣿⣿⢟⠛⠛⠿⡢⢻⣿⣾⣞⣿⡏⠖⢸⣿⢣⣷⡸⣇⣿⣿⣿⢼⡿⣿⣿ ]],
+    [[ ⣡⢿⡷⣿⣿⣾⣿⣷⣶⣮⣄⣿⣏⣸⣻⣃⠭⠄⠛⠙⠛⠳⠋⣿⣿⣇⠙⣿⢸⣿ ]],
+    [[ ⠫⣿⣧⣿⣿⣿⣿⣿⣿⣿⣿⠻⣿⣾⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣹⢷⣿⡼⠋ ]],
+    [[ ⠄⠸⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣿⣿⣿⠄⠄ ]],
+    [[ ⠄⠄⢻⢹⣿⠸⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣼⣿⣿⣿⣿⡟⠄⠄ ]],
+    [[ ⠄⠄⠈⢸⣿⠄⠙⢿⣿⣿⣹⣿⣿⣿⣿⣟⡃⣽⣿⣿⡟⠁⣿⣿⢻⣿⣿⢿⠄⠄ ]],
+    [[ ⠄⠄⠄⠘⣿⡄⠄⠄⠙⢿⣿⣿⣾⣿⣷⣿⣿⣿⠟⠁⠄⠄⣿⣿⣾⣿⡟⣿⠄⠄ ]],
+    [[ ⠄⠄⠄⠄⢻⡇⠸⣆⠄⠄⠈⠻⣿⡿⠿⠛⠉⠄⠄⠄⠄⢸⣿⣇⣿⣿⢿⣿⠄⠄ ]],
+*/
+/* #pragma GCC optimize(2) */
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define i64 long long
+#define Debug(x) cout << #x << ':' << x << endl
+#define all(x) (x).begin(), (x).end()
+
+struct stu {
+    int a;
+    int b;
+    int c;
+    int sum;
+    int index;
+};
+
+bool cmp(stu x, stu y) {
+    if (x.sum == y.sum) {
+        if (x.a == y.a) {
+            if (x.b == y.b) {
+                return x.c < y.c;
+            }
+            return x.b < y.b;
+        }
+        return x.a > y.a;
+    }
+    return x.sum > y.sum;
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    cin >> n;
+    vector<stu> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i].a >> a[i].b >> a[i].c;
+        a[i].index = i + 1;
+        a[i].sum = a[i].a + a[i].b + a[i].c;
+    }
+    sort(all(a), cmp);
+    for (int i = 0; i < 5; i++) {
+        cout << a[i].index << ' ' << a[i].sum << endl;
+    }
+    return 0;
+}
